9f69b87ee1e0bb2e742e9ca44b1b67df1d1533f8
KristianWB <42993999+KristianWB@users.noreply.github.com>
2018-09-27 08:39:04 +0200

    Initial commit

---
+++ src/DisplayThreeMessages.java
@@ -0,0 +1,8 @@
+public class DisplayThreeMessages {
+    public static void main(String[] args)   {
+        System.out.println("Welcome to Java!");
+        System.out.println("Learning Java is fun");
+        System.out.println("Programming is fun");
+    }
+
+}
